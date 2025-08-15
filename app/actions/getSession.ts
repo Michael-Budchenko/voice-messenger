6f90764ea155599c0a62cf2e763741616093dd69
Michael-Budchenko <cupnick1@gmail.com>
2025-08-15 22:00:52 +0300

    done

--- app/actions/getSession.ts
+++ app/actions/getSession.ts
@@ -1,10 +1,3 @@
-// import { getServerSession } from 'next-auth';
-// import { authOptions } from '../api/auth/[...nextauth]/route';
-
-// export default async function getSession() {
-//   return await getServerSession(authOptions);
-// }
-
 import { authOptions } from '@/app/libs/auth';
 import { getServerSession } from 'next-auth';
 
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,3 @@
-// import { dirname } from "path";
-// import { fileURLToPath } from "url";
-// import { FlatCompat } from "@eslint/eslintrc";
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
-
-// const compat = new FlatCompat({
-//   baseDirectory: __dirname,
-// });
-
-// const eslintConfig = [
-//   ...compat.extends("next/core-web-vitals", "next/typescript"),
-
-// ];
-
-// export default eslintConfig;
-
 import { FlatCompat } from '@eslint/eslintrc';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
@@ -30,12 +12,10 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
 
-  // Ігноруємо повністю всі згенеровані Prisma файли
   {
     ignores: ['app/generated/prisma/**/*'],
   },
 
-  // Глобальні правила для всіх TS/TSX файлів
   {
     files: ['**/*.{ts,tsx}'],
     rules: {
